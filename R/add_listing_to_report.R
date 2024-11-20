35085f57c1c6bb568397a93d5a2c55a35ed7f969
shummel <steve.hummel@simulations-plus.com>
2024-11-20 15:05:42 -0500

    added listing_table function to allow reporter::define() modifications

--- R/add_listing_to_report.R
+++ R/add_listing_to_report.R
@@ -1,27 +1,81 @@
+
+#' make a listing table
+#'
+#' @description
+#' `listing_table()`
+#' is used to build listing tables for each deletion data subset
+#' listing tables can then be modified further using [reporter::define()] or sent to [add_listing_to_report()] where
+#' listing tables are the second argument
+#' @param listing Data to be used in the listing. Subset to variables that best portray why the data were deleted.
+#' Selected variables should be source variables if possible
+#' @param ... additional arguments passed to [reporter::create_table()]
+#' @param title Title to describe each unique listing in the report. Usually closely resembles DELFNC
+#' @return A deletion report object that includes all added listings and their titles of class "table_spec" & "list"
+#' @export
+#' @examples
+#' \dontrun{
+#'
+#'  # Add tables to the report
+#'
+#'del_report <- del_report %>%
+#'  add_listing_to_report(
+#'    listing_table(
+#'      pk_delimp_mod  %>%
+#'        filter(DELFN == 5, DVID == 2) %>%
+#'        arrange(STUDYID, USUBJID, DTTM) %>%
+#'        select(ROW,USUBJID, VISIT, PCTPT, PCTESTCD, PCSTAT),
+#'      "Compound A (ng/mL), Sample Not Done / Not Recorded"
+#'    ) %>%
+#'      define(ROW, label = " ", width = 0.15, id_var = TRUE, align = "left")
+#'  )  %>%
+#'  add_listing_to_report(
+#'    listing_table(
+#'      pk_delimp_mod %>%
+#'        filter(DELFN == 5, DVID == 3) %>%
+#'        arrange(STUDYID, USUBJID, DTTM) %>%
+#'        select(ROW,USUBJID, VISIT, PCTPT, PCTESTCD, PCSTAT),
+#'      "Compound B (ng/mL), Sample Not Done / Not Recorded"
+#'    ))
+#'
+#'
+#'}
+
+listing_table <- function(listing,title,...){
+
+  if(missing(title)){
+    cli::cli_abort(text = "please provide a title for your listing table")
+  }
+
+
+  reporter::create_table(listing,
+                         borders = "none",
+                         use_attributes=c("format","label"),
+                         headerless = FALSE,
+                         continuous = TRUE,
+                         first_row_blank = FALSE,...) %>%
+    reporter::titles(width="page",align = "left",title)
+
+}
+
+
+
 #' Add each listing to the initialized report
 #'
 #' @description
 #' `add_listing_to_report()`
 #' is used to build listings one deletion flag at a time
 #'
-#' @param deletion_report Name of initialized deletion report made with `build_report()`
+#' @param deletion_report Name of initialized deletion report made with [build_report()]
 #' @param listing Data to be used in the listing. Subset to variables that best portray why the data were deleted.
 #' Selected variables should be source variables if possible
-#' @param ... additional arguments passed to reporter::create_table()
-#' @param title Title to describe each unique listing in the report. Usually closely resembles DELFNC
+#' @param ... additional arguments passed to [reporter::create_table()]
 #' @return A deletion report object that includes all added listings and their titles
+#' @export
 #' @examples
 #' \dontrun{
 #' # A deletion report with DELFN 1 and 9 added
 #'
 #'
-#' del_report <- del_report %>%
-#'  add_listing_to_report(
-#'  listing = pd_delimp %>%
-#'    filter(DELFN == 1) %>%
-#'    select(STUDY, USUBJID, VISIT, DV),
-#'  title = "Empty Place Holder Records"
-#' ) %>%
 #'  add_listing_to_report(
 #'    listing = pd_delimp %>%
 #'      filter(DELFN == 9) %>%
@@ -34,7 +88,7 @@
 #'
 
 
-add_listing_to_report=function(deletion_report,listing,title,...){
+add_listing_to_report <- function(deletion_report,listing,...){
 
   assertthat::assert_that(!purrr::is_null(deletion_report),
                           msg = "Build deletion report first")
@@ -45,6 +99,9 @@ add_listing_to_report=function(deletion_report,listing,title,...){
   existing_titles <- purrr::map_chr(which(seq_along(deletion_report$content)%%2==1),
                                     ~deletion_report$content[[.x]]$object$titles[[1]][[1]])
 
+
+  title <- listing$titles[[1]]$titles
+
   if(title %in% existing_titles){
     cli::cli_warn(c("Table {.val {title}} already exists in listings report",
                     "1. Verify data have not been added",
@@ -52,24 +109,18 @@ add_listing_to_report=function(deletion_report,listing,title,...){
   }
 
   # early return
-  if(nrow(listing)==0){
+  if(nrow(listing$data)==0){
     cli::cli_inform(c("i"="{.val {title}} has 0 rows and will be ignored"))
     return(deletion_report)
   }
 
-  # TODO Discuss use_attributes
-  new_table <- reporter::create_table(listing,
-                                      borders = "none",
-                                      use_attributes=c("format","label"),
-                                      headerless = FALSE,
-                                      continuous = TRUE,
-                                      first_row_blank = FALSE,
-                                      ...) %>%
-    reporter::titles(width="page",align = "left",title)
+  # broke out the create_table so that users can customize
+  # individual tables and variables more easily using reporter::define()
+  new_table <- listing
 
 
   # Row counts
-  row_count <- reporter::create_text(paste("",paste0("N=",nrow(listing)),sep="\n"),
+  row_count <- reporter::create_text(paste("",paste0("N=",nrow(listing$data)),sep="\n"),
                                      width = NULL, align = "left",
                                      borders = "none")
   # Deletion listings
