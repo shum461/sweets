d642cf6470884314e04bbc41e14c91cc16a5553c
shummel <steve.hummel@simulations-plus.com>
2025-01-21 17:24:34 -0500

    added roxygen

--- R/check_models.R
+++ R/check_models.R
@@ -1,4 +1,22 @@
-
+#' Check if modeling in underway
+#'
+#' @param paths to model files. Defaults to `paths=c("../monolix/","../nm/")`
+#' Provide the path(s) if you're not working in asmbdat directory
+#'
+#' @description Sometimes it is useful for programmers to know if and when modeling has occurred
+#' @return A data frame with software,output,user,min and max modification times
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # if working in asmbdat and looking just for monolix or nonmem, no arguments needed.
+#'   check_models()
+#'
+#' # otherwise provide the path(s)
+#' check_models(path = "../misc/sponsor/sponsor-108/123456/d1pkpd/monolix/")
+#' }
+#' @export
+#'
 
 check_models <- function(paths=c("../monolix/","../nm/")){
 
